76c273edb627362da426a0c364c73b5ac1b3244c
babywyrm <55672787+babywyrm@users.noreply.github.com>
2020-02-07 20:36:53 -0600

    Create pyth_env.py

---
+++ pyth_env.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#########
+#########
+##
+##  check_python_env_and_more_
+##
+##############################
+
+import sys
+from sys import argv
+
+print (f'''
+Python version installed: {sys.version}
+Python running on platforn: {sys.platform}
+''')
+
+if len(argv) == 10:
+        sys.exit('error: too many arguments')
+
+print(f'''
+argv = {argv}
+len(argv) = {len(argv)}
+''')
+
+print ('Printing to stdout')
+print (f'>> {sys.stderr}, "Printing to stderr"')
+
+print(f'''
+total modules search path: {len(sys.path)}
+total modules loaded: {len(sys.modules)}
+''')
+
+# success
+sys.exit(0)
