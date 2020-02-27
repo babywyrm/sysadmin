4eee6477b0ce4961614493eb0c3f5c9af04ab33b
babywyrm <55672787+babywyrm@users.noreply.github.com>
2020-02-26 18:42:03 -0600

    Create postgresql-hook-postbackup.sh

---
+++ pgbackup/postgresql-hook-postbackup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+INFILES="$@"
+OUTFILE="/tmp/dbdumps-$HOSTNAME-$(date +%Y%m%d-%H%M%S).tgz.aes"
+
+tar czpf - $INFILES | openssl aes-256-cbc -salt -out "$OUTFILE"
+
+echo $OUTFILE
+
+############
+##
+##
+###########################################
