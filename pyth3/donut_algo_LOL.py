da1c9425e9e7254a5f8060dcfeac12baa40ddd30
babywyrm <55672787+babywyrm@users.noreply.github.com>
2020-03-21 16:17:39 -0500

    Update donut_algo_LOL.py

--- pyth3/donut_algo_LOL.py
+++ pyth3/donut_algo_LOL.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-##
+##  "I'll Take The Lot" 
+###### (HP & the Bear)
 ##
 ##  lol, I found this and made sure it could pyth_3
 ##  definitely from something somewhere onilne but why
@@ -8,6 +9,7 @@
 ##  why.
 ##
 ##  Algorithm to calculate the price of any number of timbits
+##
 ##  get the input number of timbits
 ##  keep track of the total cost, starting from zero
 ##  buy as many large boxes as you can
@@ -16,7 +18,9 @@
 ##  buy a medium box if you can and repeat steps A. and B.
 ##  buy a small box if you can and repeat steps A. and B.
 ##  buy individual timbits and repeat step B.
+##
 ##  output the total cost
+##
 #########################
 ##
 ##
@@ -24,13 +28,16 @@
 timbitsLeft = int(input("How many timbits you got bro..."))
 totalCost = 0           
 
-# step 3: buy as many large boxes as you can
+## grab as many large boxes as you can!
+
 if timbitsLeft >= 40:
    BigBox = int(timbitsLeft / 40)
    totalCost = totalCost + BigBox * 6.19
    timbitsLeft = timbitsLeft - 40 * BigBox
 
 ##########################################
+
+## grab medium boxes, repeat steps to recalc
 
 if timbitsLeft >= 20: 
     MedBox = int(timbitsLeft / 20)           
@@ -39,12 +46,16 @@ if timbitsLeft >= 20:
 
 ##########################################
 
+## grab baby boxes, repeat steps, & finale
+
 if timbitsLeft >= 10:                
     BabyBox = int(timbitsLeft / 10)
     totalCost = totalCost + BabyBox * 1.99
     timbitsLeft = timbitsLeft - 10 * BabyBox 
 
 ##########################################
+
+## duh, add the stragglers
 
 totalCost = totalCost + timbitsLeft * 0.2
 
