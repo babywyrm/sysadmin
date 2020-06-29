6a87acf9c420e45f128b1328a7126a5c0d3cde73
babywyrm <55672787+babywyrm@users.noreply.github.com>
2020-06-29 16:53:53 -0500

    Create setup.py

---
+++ ACAD/assault/setup.py
@@ -0,0 +1,17 @@
+## partial, probably
+##
+###############################################
+
+# Package meta-data.
+
+NAME = 'assault'
+DESCRIPTION = 'A Python based web load testing tool.'
+URL = 'https://github.com/example/assault'
+EMAIL = 'me@example.com'
+AUTHOR = 'Example Person'
+REQUIRES_PYTHON = '>=3.7.0'
+VERSION = '0.1.0'
+
+###############################################
+##
+##
