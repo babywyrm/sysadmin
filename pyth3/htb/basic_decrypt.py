f2eb55d9dd8e68f6c8eef8cf64efdfc7090740ec
babywyrm <55672787+babywyrm@users.noreply.github.com>
2021-06-05 14:59:56 -0700

    Update and rename basic_enc.py to basic_decrypt.py

--- pyth3/htb/basic_decrypt.py
+++ pyth3/htb/basic_decrypt.py
@@ -8,15 +8,15 @@
 ##
 
 def decryption(msg):
-  pt = []
-  for char in msg:
-    char = char — 18
-    char = 179 * char % 256
-    pt.append(char)
-  return bytes(pt)
+    pt = []
+    for char in msg:
+        char = char — 18
+        char = 179 * char % 256
+        pt.append(char)
+     return bytes(pt)
   
 with open(‘msg.enc’) as f:
-  ct = bytes.fromhex(f.read())
+    ct = bytes.fromhex(f.read())
 
 pt = decryption(ct)
 print(pt)
