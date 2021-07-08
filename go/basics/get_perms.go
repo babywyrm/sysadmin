a88e8819f123bae83425b2034514ab23919adc99
babywyrm <55672787+babywyrm@users.noreply.github.com>
2021-07-07 19:03:53 -0700

    Create get_perms.go

---
+++ go/basics/get_perms.go
@@ -0,0 +1,28 @@
+//obvi
+//
+//
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide an argument when running from the CLI eg '/bin/ls'")
+		os.Exit(1)
+	}
+
+	file := arguments[1]
+
+	info, err := os.Stat(file)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	mode := info.Mode()
+	fmt.Print(file, ": ", mode, "n")
+}
