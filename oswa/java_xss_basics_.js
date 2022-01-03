e2434b2a3fd01ba48e829e0d8a4ee880f112926a
babywyrm <55672787+babywyrm@users.noreply.github.com>
2022-01-02 20:17:11 -0800

    Update java_xss_basics_.js

--- oswa/java_xss_basics_.js
+++ oswa/java_xss_basics_.js
@@ -2,14 +2,14 @@
 
 // https://blog.sessionstack.com/how-javascript-works-5-types-of-xss-attacks-tips-on-preventing-them-e6e28327748a
 //
-++++++++++++++++
+///////////////////
 
 let inputs = document.getElementsByTagName("input") 
 
 for (let input of inputs){
     console.log(input.value)}
 
-++++++++++++++++
+///////////////////
 
 function logKey(event){
   console.log(event.key)
@@ -17,4 +17,4 @@ function logKey(event){
 
 document.addEventListener('keydown', logKey);
 
-++++++++++++++++
+///////////////////
