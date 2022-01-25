bf711bc6ebf360bafc3d79db36d6276682eacd30
babywyrm <55672787+babywyrm@users.noreply.github.com>
2022-01-24 19:58:54 -0800

    Create forms.py

---
+++ flask/base/examples/forms.py
@@ -0,0 +1,56 @@
+#########################
+
+from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField, validators, ValidationError, PasswordField
+from models import db, User
+
+class ContactForm(Form):
+  name = TextField("Name",  [validators.Required("Please enter your name.")])
+  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  subject = TextField("Subject",  [validators.Required("Please enter a subject.")])
+  message = TextAreaField("Message",  [validators.Required("Please enter a message.")])
+  submit = SubmitField("Send")
+
+class SignupForm(Form):
+  firstname = TextField("First name",  [validators.Required("Please enter your first name.")])
+  lastname = TextField("Last name",  [validators.Required("Please enter your last name.")])
+  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  password = PasswordField('Password', [validators.Required("Please enter a password.")])
+  submit = SubmitField("Create account")
+
+  def __init__(self, *args, **kwargs):
+    Form.__init__(self, *args, **kwargs)
+
+  def validate(self):
+    if not Form.validate(self):
+      return False
+    
+    user = User.query.filter_by(email = self.email.data.lower()).first()
+    if user:
+      self.email.errors.append("That email is already taken")
+      return False
+    else:
+      return True
+
+class SigninForm(Form):
+  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  password = PasswordField('Password', [validators.Required("Please enter a password.")])
+  submit = SubmitField("Sign In")
+  
+  def __init__(self, *args, **kwargs):
+    Form.__init__(self, *args, **kwargs)
+
+  def validate(self):
+    if not Form.validate(self):
+      return False
+    
+    user = User.query.filter_by(email = self.email.data).first()
+    if user and user.check_password(self.password.data):
+      return True
+    else:
+      self.email.errors.append("Invalid e-mail or password")
+      return False
+    
+    
+#################################
+##
+##
