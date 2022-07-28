ca83cb34f0d075ff935eb5ba29d3305928ff51ac
babywyrm <55672787+babywyrm@users.noreply.github.com>
2022-07-27 19:17:14 -0700

    Create jsonhttpserv_.py

---
+++ jq/json/jsonhttpserv_.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+#############
+##
+##
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse
+import json
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(json.dumps({
+            'method': self.command,
+            'path': self.path,
+            'real_path': parsed_path.query,
+            'query': parsed_path.query,
+            'request_version': self.request_version,
+            'protocol_version': self.protocol_version
+        }).encode())
+        return
+
+    def do_POST(self):
+        content_len = int(self.headers.getheader('content-length'))
+        post_body = self.rfile.read(content_len)
+        data = json.loads(post_body)
+
+        parsed_path = urlparse(self.path)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(json.dumps({
+            'method': self.command,
+            'path': self.path,
+            'real_path': parsed_path.query,
+            'query': parsed_path.query,
+            'request_version': self.request_version,
+            'protocol_version': self.protocol_version,
+            'body': data
+        }).encode())
+        return
+
+if __name__ == '__main__':
+    server = HTTPServer(('localhost', 8000), RequestHandler)
+    print('Starting server at http://localhost:8000')
+    server.serve_forever()
+
+##########################
+##
+##    
