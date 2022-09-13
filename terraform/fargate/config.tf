a4a75faef7dec0fb9fcebed85b4da55dd471a292
babywyrm <55672787+babywyrm@users.noreply.github.com>
2022-09-12 20:32:20 -0700

    Create config.tf

---
+++ terraform/fargate/config.tf
@@ -0,0 +1,23 @@
+# config.tf
+provider "aws" {
+  region  = "us-east-1"
+  profile = "tfuser"
+}
+
+terraform {
+  required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket  = "terraform"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    profile = "tfuser"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.69.0"
+    }
+  }
+}
