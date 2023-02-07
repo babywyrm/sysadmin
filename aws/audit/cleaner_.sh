662939428a7ee50502866a519bf05b4bf6624a96
babywyrm <55672787+babywyrm@users.noreply.github.com>
2023-02-06 20:02:28 -0800

    Update cleaner_.sh

--- aws/audit/cleaner_.sh
+++ aws/audit/cleaner_.sh
@@ -3,6 +3,18 @@
 ##
 ##
 
+# install aws cli first and configure it with credentials and default region
+# the script will iterate over all regions of AWS
+
+#for region in `aws ec2 describe-regions --output text | cut -f4`
+#do
+#     echo -e "\nListing Instances in region:'$region'..."
+#     aws ec2 describe-instances --query "Reservations[*].Instances[*].{IP:PublicIpAddress,ID:InstanceId,Type:InstanceType,State:State.Name,Name:Tags[0].Value}" --output=table --region $region
+#done
+
+###########
+###########
+
 USA_ACCOUNTS=("something" "something-else "somewhere" "bye")
 
 echo "Clean it..."
@@ -31,9 +43,6 @@ sleep 1
 #################
 
 EU_ACCOUNTS=("eu1-prod-ro" "eu1-dr-prod-ro")
-
-
-
 
 #################
 ##
