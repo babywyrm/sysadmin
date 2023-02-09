33ff1747b7f53f069adb8db685de6c34ceb23e96
babywyrm <55672787+babywyrm@users.noreply.github.com>
2023-02-08 18:25:13 -0800

    Update pull_csv_.py

--- git/pull_csv_.py
+++ git/pull_csv_.py
@@ -1,3 +1,25 @@
+# first: mkdir user && cd user && cp /path/to/get_gists.py .
+# python3 get_gists.py user
+import requests
+import sys
+from subprocess import call
+
+user = sys.argv[1]
+
+r = requests.get('https://api.github.com/users/{0}/gists'.format(user))
+
+for i in r.json():
+	call(['git', 'clone', i['git_pull_url']])
+
+	description_file = './{0}/description.txt'.format(i['id'])
+	with open(description_file, 'w') as f:
+		f.write('{0}\n'.format(i['description']))
+        
+##
+##
+##############
+##############
+
 import numpy as np
 import pandas as pd
 import requests
