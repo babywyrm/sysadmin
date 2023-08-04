34bfed35211132dec59f9b2313f055fad9a9ec3c
babywyrm <55672787+babywyrm@users.noreply.github.com>
2023-08-03 18:47:13 -0700

    Create analyze_spring_.py

---
+++ reviews/analyze_spring_.py
@@ -0,0 +1,103 @@
+import os
+import subprocess
+
+def run_command(command, cwd=None):
+    """Helper function to run a shell command."""
+    return subprocess.run(command, shell=True, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+def detect_secrets_scan(repo_path):
+    """Run detect-secrets on the given repository directory."""
+    print("Running detect-secrets scan...")
+    command = f"detect-secrets scan --exclude-files '({repo_path}/.git/*|.*/target/.*|.*/build/.*|.*/out/.*|.*/bin/.*|.*/node_modules/.*|.*/bower_components/.*)'"
+    result = run_command(command, repo_path)
+    return result.stdout
+
+def trufflehog_scan(repo_path):
+    """Run truffleHog on the given repository directory."""
+    print("Running truffleHog scan...")
+    command = f"trufflehog --entropy=False {repo_path}"
+    result = run_command(command)
+    return result.stdout
+
+def git_leaks_scan(repo_path):
+    """Run git leaks on the given repository directory."""
+    print("Running git leaks scan...")
+    command = f"git leaks --no-metadata -q {repo_path}"
+    result = run_command(command)
+    return result.stdout
+
+def find_sensitive_strings(repo_path):
+    """Search for sensitive strings in the Java files of the given repository directory."""
+    print("Searching for sensitive strings...")
+    sensitive_strings = []
+    for root, _, files in os.walk(repo_path):
+        for filename in files:
+            if filename.endswith('.java'):
+                file_path = os.path.join(root, filename)
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    content = file.read()
+                    # Look for commonly sensitive strings in Java Spring Boot applications
+                    common_sensitive_strings = [
+                        "jdbc:mysql://",
+                        "jdbc:postgresql://",
+                        "jdbc:oracle://",
+                        "jdbc:sqlserver://",
+                        "rmi://",
+                        "authentication",
+                        "BasicAuth",
+                        "OAuth2",
+                        "userInput",
+                        "password",
+                        "secret",
+                        "apikey",
+                        "accessToken",
+                        "token",
+                        "privateKey",
+                        "dbPassword",
+                        "secretKey",
+                        "apiKey",
+                        "authToken",
+                        "credentials",
+                        "dbUsername",
+                        "dbConnectionString",
+                        "serviceURL",
+                        "serviceEndpoint",
+                        "url",
+                    ]
+                    for string in common_sensitive_strings:
+                        if string.lower() in content.lower():
+                            sensitive_strings.append(f"Found '{string}' in file: {file_path}")
+
+    return sensitive_strings
+
+def main():
+    repo_path = "/path/to/your/java/spring/boot/repo"  # Replace this with the actual path to your repo
+
+    # Run scans
+    detect_secrets_output = detect_secrets_scan(repo_path)
+    trufflehog_output = trufflehog_scan(repo_path)
+    git_leaks_output = git_leaks_scan(repo_path)
+
+    # Find sensitive strings
+    sensitive_strings = find_sensitive_strings(repo_path)
+
+    # Display the results
+    print("\n=== Detect Secrets Scan Results ===\n")
+    print(detect_secrets_output)
+
+    print("\n=== TruffleHog Scan Results ===\n")
+    print(trufflehog_output)
+
+    print("\n=== Git Leaks Scan Results ===\n")
+    print(git_leaks_output)
+
+    print("\n=== Sensitive Strings Found ===\n")
+    for sensitive_string in sensitive_strings:
+        print(sensitive_string)
+
+if __name__ == "__main__":
+    main()
+
+#######################
+##
+##
