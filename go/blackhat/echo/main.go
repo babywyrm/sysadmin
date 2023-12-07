e7acda714af0884726d03a1a04a83b9dc6d397dd
babywyrm <55672787+babywyrm@users.noreply.github.com>
2023-12-06 19:15:50 -0800

    Create main.go

---
+++ go/blackhat/echo/main.go
@@ -0,0 +1,61 @@
+//
+//
+
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+// echo is a handler function that simply echoes received data.
+func echo(conn net.Conn) {
+	defer conn.Close()
+
+	// Create a buffer to store received data.
+	b := make([]byte, 512)
+	for {
+		// Receive data via conn.Read into a buffer.
+		size, err := conn.Read(b[0:])
+		if err != nil && err != io.EOF {
+			log.Println("Unexpected error")
+			break
+		}
+
+		if err == io.EOF && size == 0 {
+			log.Println("Client disconnected")
+			break
+		}
+
+		log.Printf("Received %d bytes: %s", size, string(b))
+
+		// Send data via conn.Write.
+		log.Println("Writing data")
+		if _, err := conn.Write(b[0:size]); err != nil {
+			log.Fatalln("Unable to write data")
+		}
+	}
+}
+
+func main() {
+	// bind to TCP port 20080 everywhere
+	listener, err := net.Listen("tcp", ":20080")
+	if err != nil {
+		log.Fatalln("Unable to bind to port")
+	}
+	log.Println("Listening on 0.0.0.0:20080")
+	for {
+		// Wait for connection. Create net.Conn on connection established.
+		conn, err := listener.Accept()
+		log.Println("Received connection")
+		if err != nil {
+			log.Fatalln("Unable to accept connection")
+		}
+		// Handle the connection. Using goroutine for concurrency.
+		go echo(conn)
+	}
+}
+
+//
+//
