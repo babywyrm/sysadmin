269984a9b98a2efb24a3a51ae5578f8fe0f55fb8
babywyrm <55672787+babywyrm@users.noreply.github.com>
2024-03-07 19:47:32 -0800

    Update old___.py

--- pyth3/crypt/old___.py
+++ pyth3/crypt/old___.py
@@ -1,4 +1,22 @@
-#!/usr/bin/python
+#! /usr/bin/env python
+
+import re, os, sys
+##
+##
+maps_file = open("/proc/self/maps", 'r')
+mem_file = open("/proc/self/mem", 'rb', 0)
+output_file = open("self.dump", 'wb')
+for line in maps_file.readlines():  # for each mapped region
+    m = re.match(r'([0-9A-Fa-f]+)-([0-9A-Fa-f]+) ([-r])', line)
+    if m.group(3) == 'r':  # if this is a readable region
+        start = int(m.group(1), 16)
+        end = int(m.group(2), 16)
+        mem_file.seek(start)  # seek to region start
+        chunk = mem_file.read(end - start)  # read region contents
+        output_file.write(chunk)  # dump contents to standard output
+maps_file.close()
+mem_file.close()
+output_file.close()
 
 ##
 ##
