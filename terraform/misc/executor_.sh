f3fbb6160dcd92971059ef32b9e431fa118141a8
babywyrm <55672787+babywyrm@users.noreply.github.com>
2024-06-15 20:36:30 -0700

    Update executor_.sh

--- terraform/misc/executor_.sh
+++ terraform/misc/executor_.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+
 # Deploy script to manage Terraform lifecycle
 
 # Function to handle cleanup
@@ -23,6 +23,17 @@ print_public_ip() {
     echo "Public IP address of instance ${instance_id}: ${public_ip}"
 }
 
+# Function to print countdown timer in seconds
+print_timer() {
+    total_seconds=$1
+    while [ $total_seconds -gt 0 ]; do
+        printf "\rTime remaining: %d seconds" $total_seconds
+        sleep 1
+        ((total_seconds--))
+    done
+    echo ""
+}
+
 # Function to handle main deployment and lifecycle
 deploy() {
     # Initialize Terraform (if not already initialized)
@@ -33,14 +44,15 @@ deploy() {
 
     # Get the duration for the instance to be up (in minutes)
     duration_minutes=$1
+    duration_seconds=$((duration_minutes * 60))
 
     # Print instance public IP
     instance_id=$(terraform state show aws_instance.my_instance | grep id | awk '{print $3}')
     print_public_ip "${instance_id}"
 
-    # Sleep for the specified duration
-    echo "Vulnerable Instance will be up for $duration_minutes minutes..."
-    sleep "$((duration_minutes * 60))"  # Convert minutes to seconds
+    # Print countdown timer
+    echo "Instance will be up for $duration_minutes minutes..."
+    print_timer $duration_seconds
 
     # Call destroy_and_cleanup function
     destroy_and_cleanup
