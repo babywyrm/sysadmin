40dab085d161941a27e1cc9e534fd735252b6ba0
babywyrm <55672787+babywyrm@users.noreply.github.com>
2025-03-02 11:07:02 -0800

    Update check_beta_.go

--- docker/cgroups/check_beta_.go
+++ docker/cgroups/check_beta_.go
@@ -1,5 +1,7 @@
 //
 // beta for a reason, tbh
+// also, docker stats --no-stream
+// also, https://github.com/google/cadvisor
 // 
 
 package main
@@ -227,3 +229,30 @@ func main() {
 		time.Sleep(*interval)
 	}
 }
+
+/*
+module dock-monitor
+
+go 1.18
+
+require github.com/docker/docker v20.10.17+incompatible
+ 
+replace github.com/docker/distribution => github.com/docker/distribution v2.7.1+incompatible
+
+require (
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/docker/distribution v2.8.3+incompatible // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/moby/term v0.5.2 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/time v0.10.0 // indirect
+	gotest.tools/v3 v3.5.2 // indirect
+)
+*/
