be230d6bd4cd8a30ced26629e4b0b7985f964a6e
babywyrm <55672787+babywyrm@users.noreply.github.com>
2025-07-17 21:17:59 -0700

    Update vimrc_BETTER_.sh

--- vimrc_BETTER_.sh
+++ vimrc_BETTER_.sh
@@ -1,37 +1,48 @@
-" Don't try to be vi compatible
+#!/bin/bash
+
+set -e
+
+echo "[+] Creating ~/.vim/autoload for vim-plug..."
+mkdir -p ~/.vim/autoload ~/.vim/plugged
+
+echo "[+] Installing vim-plug..."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "[+] Writing ~/.vimrc..."
+cat <<EOF > ~/.vimrc
 set nocompatible
 
-" Helps force plugins to load correctly when it is turned back on below
-filetype off
+call plug#begin('~/.vim/plugged')
 
-" TODO: Load plugins here (pathogen or vundle)
+Plug 'sheerun/vim-polyglot'
+Plug 'dense-analysis/ale'
+Plug 'jmcantrell/vim-virtualenv'
+Plug 'altercation/vim-colors-solarized'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
 
-" Turn on syntax highlighting
-syntax on
+call plug#end()
 
-" For plugins to load correctly
 filetype plugin indent on
+syntax enable
 
-" TODO: Pick a leader key
-" let mapleader = ","
+let mapleader = ","
 
-" Security
-set modelines=0
-
-" Show line numbers
-set number
-
-" Show file stats
-set ruler
-
-" Blink cursor on error instead of beeping (grr)
-set visualbell
-
-" Encoding
 set encoding=utf-8
-
-" Whitespace
+set number
+set ruler
+set showmode
+set showcmd
+set laststatus=2
+set ttyfast
+set scrolloff=3
+set visualbell
 set wrap
+
+nnoremap j gj
+nnoremap k gk
+
 set textwidth=79
 set formatoptions=tcqrn1
 set tabstop=2
@@ -40,62 +51,55 @@ set softtabstop=2
 set expandtab
 set noshiftround
 
-" Cursor motion
-set scrolloff=3
-set backspace=indent,eol,start
-set matchpairs+=<:> " use % to jump between pairs
-runtime! macros/matchit.vim
-
-" Move up/down editor lines
-nnoremap j gj
-nnoremap k gk
-
-" Allow hidden buffers
-set hidden
-
-" Rendering
-set ttyfast
-
-" Status bar
-set laststatus=2
-
-" Last line
-set showmode
-set showcmd
-
-" Searching
-nnoremap / /\v
-vnoremap / /\v
 set hlsearch
 set incsearch
 set ignorecase
 set smartcase
 set showmatch
-map <leader><space> :let @/=''<cr> " clear search
+nnoremap / /\\v
+vnoremap / /\\v
+nnoremap <leader><space> :nohlsearch<CR>
 
-" Remap help key.
-inoremap <F1> <ESC>:set invfullscreen<CR>a
+set backspace=indent,eol,start
+set matchpairs+=<:>
+runtime! macros/matchit.vim
+
+set hidden
+set modelines=0
+
+set listchars=tab:▸\\ ,eol:¬
+nnoremap <leader>l :set list!<CR>
+
 nnoremap <F1> :set invfullscreen<CR>
 vnoremap <F1> :set invfullscreen<CR>
+inoremap <F1> <ESC>:set invfullscreen<CR>a
 
-" Textmate holdouts
+nnoremap <leader>q gqip
 
-" Formatting
-map <leader>q gqip
-
-" Visualize tabs and newlines
-set listchars=tab:▸\ ,eol:¬
-" Uncomment this to enable by default:
-" set list " To enable by default
-" Or use your leader key + l to toggle on/off
-map <leader>l :set list!<CR> " Toggle tabs and EOL
-
-" Color scheme (terminal)
 set t_Co=256
 set background=dark
 let g:solarized_termcolors=256
 let g:solarized_termtrans=1
-" put https://raw.github.com/altercation/vim-colors-solarized/master/colors/solarized.vim
-" in ~/.vim/colors/ and uncomment:
-" colorscheme solarized
+colorscheme solarized
 
+let g:ale_linters = {
+\\   'python': ['flake8', 'mypy', 'pylint']
+\\}
+let g:ale_fixers = {
+\\   'python': ['black', 'isort']
+\\}
+let g:ale_python_flake8_executable = 'flake8'
+let g:ale_python_flake8_options = '--max-line-length=88'
+let g:ale_python_black_executable = 'black'
+let g:ale_fix_on_save = 1
+
+let g:virtualenv_auto_activate = 1
+EOF
+
+echo "[+] Installing Solarized colorscheme for offline support..."
+git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/plugged/vim-colors-solarized || true
+
+echo "[+] Done. Now open Vim and run :PlugInstall"
+
+##
+##
