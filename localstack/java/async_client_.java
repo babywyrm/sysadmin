479cf1c46ba91ca834a676ad2cb5731d06652840
babywyrm <55672787+babywyrm@users.noreply.github.com>
2025-11-30 16:46:36 -0800

    Update async_client_.java

--- localstack/java/async_client_.java
+++ localstack/java/async_client_.java
@@ -1,4 +1,14 @@
+package your.package.name;
+
 import java.util.function.Supplier;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.client.builder.ExecutorFactory;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 
 import cloud.localstack.TestUtils;
 import cloud.localstack.docker.LocalstackDocker;
@@ -6,26 +16,40 @@ import cloud.localstack.docker.LocalstackDocker;
 public class TestAsyncUtils extends TestUtils {
 
     public static AmazonDynamoDBAsync getClientDynamoDBAsync() {
-        return AmazonDynamoDBAsyncClientBuilder.standard()
-                .withEndpointConfiguration(createEndpointConfiguration(LocalstackDocker.INSTANCE::getEndpointDynamoDB))
-                .withCredentials(getCredentialsProvider())
-                .build();
+        return buildAsyncClient(
+            AmazonDynamoDBAsyncClientBuilder.standard(),
+            LocalstackDocker.INSTANCE::getEndpointDynamoDB
+        );
     }
 
     public static AmazonSQSAsync getClientSQSAsync() {
         return getClientSQSAsync(null);
     }
 
-    public static AmazonSQSAsync getClientSQSAsync(final ExecutorFactory executorFactory) {
-        return AmazonSQSAsyncClientBuilder.standard()
-                .withEndpointConfiguration(getEndpointConfigurationSQS())
-                .withExecutorFactory(executorFactory)
-                .withCredentials(getCredentialsProvider())
-                .build();
+    public static AmazonSQSAsync getClientSQSAsync(
+        ExecutorFactory executorFactory
+    ) {
+        AmazonSQSAsyncClientBuilder builder = AmazonSQSAsyncClientBuilder
+            .standard()
+            .withEndpointConfiguration(getEndpointConfigurationSQS())
+            .withCredentials(getCredentialsProvider());
+
+        if (executorFactory != null) {
+            builder.withExecutorFactory(executorFactory);
+        }
+
+        return builder.build();
     }
 
-
-    protected static AwsClientBuilder.EndpointConfiguration createEndpointConfiguration(Supplier<String> supplier) {
-        return getEndpointConfiguration(supplier.get());
+    private static <T> T buildAsyncClient(
+        AwsClientBuilder<?, T> builder,
+        Supplier<String> endpointSupplier
+    ) {
+        return builder
+            .withEndpointConfiguration(
+                getEndpointConfiguration(endpointSupplier.get())
+            )
+            .withCredentials(getCredentialsProvider())
+            .build();
     }
 }
